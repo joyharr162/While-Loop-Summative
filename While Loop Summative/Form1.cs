14b3bb2ee3877e2f674d3c2d7198bf84e592cb86
joyharr162 <joyharr162@ed.amdsb.ca>
2016-11-29 13:30:56 -0500

    initial commit

--- While Loop Summative/Form1.cs
+++ While Loop Summative/Form1.cs
@@ -19,23 +19,19 @@ namespace While_Loop_Summative
         public Form1()
         {
             InitializeComponent();
-
-            //create pens and brushes
-            Graphics formGraphics = this.CreateGraphics();
-            Font drawFont = new Font("Arial", 16, FontStyle.Bold);
-            SolidBrush drawBrush = new SolidBrush(Color.White);
-
-            formGraphics.DrawString("Click to Begin", drawFont, drawBrush, 50, 40);
         }
 
         private void Form1_MouseClick(object sender, MouseEventArgs e)
         {
             //create pens and brushes
+            Font drawFont = new Font("Letter Gothic Std", 14, FontStyle.Bold);
+            Font secondFont = new Font("Segoe Script", 12, FontStyle.Bold);
             Graphics formGraphics = this.CreateGraphics();
             Pen drawPen = new Pen(Color.Gray, 9);
             Pen circlePen = new Pen(Color.Gray, 4);
-            Pen explodePen = new Pen(Color.OrangeRed, 4);
-            Pen explodePen2 = new Pen(Color.Orange, 4);
+            Pen explodePen = new Pen(Color.OrangeRed, 3);
+            Pen explodePen2 = new Pen(Color.Orange, 3);
+            SolidBrush briefingBrush = new SolidBrush(Color.LightGray);
             SolidBrush drawBrush = new SolidBrush(Color.Gray);
             SolidBrush blackBrush = new SolidBrush(Color.Black);
             SolidBrush yellowBrush = new SolidBrush(Color.Yellow);
@@ -43,9 +39,14 @@ namespace While_Loop_Summative
             int x = this.Width - 100;
             int y = 20;
 
+            formGraphics.DrawString("Mission Briefing", drawFont, briefingBrush, 20, 40);
+            Thread.Sleep(500);
+            formGraphics.DrawString("You will navigate fighter jet to the Death \nStar to drop a bomb through the exhaust \nport in an attempt to destroy the Death \nStar.", drawFont, drawBrush, 20, 100);
+            Thread.Sleep(3000);
+
+            //plane enter screen
             while (x > 220)
             {
-                //animate plane
                 formGraphics.FillPie(drawBrush, x, 20, 40, 40, 340, 35);
                 Thread.Sleep(10);
                 x = x - 2;
@@ -60,11 +61,11 @@ namespace While_Loop_Summative
                 formGraphics.FillRectangle(blackBrush, 240, 239, 15, 10);
             }
 
+            //drop bomb
             while (y < 230)
             {
-                //animate bomb
                 formGraphics.FillEllipse(yellowBrush, 240, y + 20, 10, 10);
-                Thread.Sleep(10);
+                Thread.Sleep(6);
                 y = y + 2;
                 Refresh();
                 //redraw star
@@ -78,15 +79,16 @@ namespace While_Loop_Summative
                 formGraphics.FillPie(drawBrush, 220, 20, 40, 40, 340, 35);
             }
 
+            //set x value to 220
             x = 220;
 
+            //plane exit screen
             while (x > 0)
             {
+                formGraphics.FillPie(drawBrush, x, 20, 40, 40, 340, 35);
+                Thread.Sleep(6);
                 //redraw bomb
                 formGraphics.FillEllipse(yellowBrush, 240, y + 20, 10, 10);
-                //animate plane
-                formGraphics.FillPie(drawBrush, x, 20, 40, 40, 340, 35);
-                Thread.Sleep(10);
                 x = x - 2;
                 Refresh();
                 //redraw star
@@ -98,14 +100,33 @@ namespace While_Loop_Summative
                 formGraphics.FillRectangle(blackBrush, 240, 239, 15, 10);
             }
 
+            //fade bomb colour
             while (x <= 255)
             {
-                //fade bomb colour
                 yellowBrush.Color = Color.FromArgb(255, 255 - x, 0);
                 formGraphics.FillEllipse(yellowBrush, 240, y + 20, 10, 10);
-                Thread.Sleep(10);
+
+                Thread.Sleep(5);
                 x = x + 1;
             }
+
+            //create explosion
+            for (int length = 0; length <= 40; length++)
+            {
+                formGraphics.DrawLine(explodePen, 260, y + 20, 260 + length, y + 20);
+                formGraphics.DrawLine(explodePen, 230, y + 20, 230 - length, y + 20);
+                formGraphics.DrawLine(explodePen, 245, y + 15, 245, y + 15 - length);
+                formGraphics.DrawLine(explodePen, 245, y + 40, 245, y + 40 + length);
+                formGraphics.DrawLine(explodePen, 260, y + 10, 260 + length, y + 10 - length);
+                formGraphics.DrawLine(explodePen, 230, y + 10, 230 - length, y + 10 - length);
+                formGraphics.DrawLine(explodePen, 260, y + 40, 260 + length, y + 40 + length);
+                formGraphics.DrawLine(explodePen, 230, y + 40, 230 - length, y + 40 + length);
+
+                Thread.Sleep(5);
+            }
+
+            formGraphics.DrawString("Good Luck!", secondFont, briefingBrush, 200, 290);
+            Thread.Sleep(500);
         }
 
         private void Form1_Load(object sender, EventArgs e)
